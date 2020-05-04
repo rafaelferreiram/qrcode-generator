6083ac1d7a75bc3c806c118ec1842496e32d4fe0
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-04 15:42:23 -0300

    refactoring endpoint

--- src/main/java/com/qrcode/generator/barcodes/QRGenBarcodeGenerator.java
+++ src/main/java/com/qrcode/generator/barcodes/QRGenBarcodeGenerator.java
@@ -6,6 +6,9 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
@@ -15,16 +18,29 @@ import com.qrcode.generator.request.BarcodeRequestDTO;
 
 public class QRGenBarcodeGenerator {
 
-	public static byte[] getQRCodeImageByteArray(BarcodeRequestDTO request) throws WriterException, IOException {
-		QRCodeWriter qrCodeWriter = new QRCodeWriter();
-		BitMatrix bitMatrix = qrCodeWriter.encode(request.format(), BarcodeFormat.QR_CODE, 350, 350);
-		Path path = FileSystems.getDefault().getPath("./QR-Codes/" + request.getName()+"_" + new Date() + ".png");
+	private static Logger logger = LoggerFactory.getLogger(QRGenBarcodeGenerator.class);
 
-		ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-		MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-		MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-		byte[] pngData = pngOutputStream.toByteArray();
+	private static String TYPE = "PNG";
+
+	private static String PATH_FOLDER = "./QR-Codes/";
+
+	public static byte[] getQRCodeImageByteArray(BarcodeRequestDTO request) throws WriterException, IOException {
+		byte[] pngData = null;
+		try {
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix bitMatrix = qrCodeWriter.encode(request.format(), BarcodeFormat.QR_CODE, 350, 350);
+			Path path = FileSystems.getDefault().getPath(PATH_FOLDER + request.getName() + "_" + new Date() + ".png");
+			logger.info("QRCode generate and saved into " + PATH_FOLDER);
+			
+			ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+			MatrixToImageWriter.writeToPath(bitMatrix, TYPE, path);
+			MatrixToImageWriter.writeToStream(bitMatrix, TYPE, pngOutputStream);
+			
+			pngData = pngOutputStream.toByteArray();
+		} catch (Exception e) {
+			logger.error("Error while generating QRCODE.", e.getMessage());
+		}
 		return pngData;
-		
+
 	}
 }
--- src/main/java/com/qrcode/generator/controller/BarcodesController.java
+++ src/main/java/com/qrcode/generator/controller/BarcodesController.java
@@ -1,6 +1,7 @@
 package com.qrcode.generator.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,12 @@ import com.qrcode.generator.request.BarcodeRequestDTO;
 public class BarcodesController {
 
 	@PostMapping(value = "/qrcode", produces = MediaType.IMAGE_PNG_VALUE)
-	public byte[] qrgenQRCodePost(@RequestBody BarcodeRequestDTO request) throws Exception {
+	public ResponseEntity<byte[]> qrgenQRCodePost(@RequestBody BarcodeRequestDTO request) throws Exception {
 		byte[] qrCodeImageByteArray = QRGenBarcodeGenerator.getQRCodeImageByteArray(request);
-		return qrCodeImageByteArray;
+		if (qrCodeImageByteArray == null) {
+			return ResponseEntity.badRequest().body(qrCodeImageByteArray);
+		}
+		return ResponseEntity.ok().body(qrCodeImageByteArray);
 	}
 
 }
