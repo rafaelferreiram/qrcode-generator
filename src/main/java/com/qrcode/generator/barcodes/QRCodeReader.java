944cf2eb845c4aa66657a32d5b2f2973363ce2fe
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-04 14:38:44 -0300

    qrcode reader

---
+++ src/main/java/com/qrcode/generator/barcodes/QRCodeReader.java
@@ -0,0 +1,34 @@
+package com.qrcode.generator.barcodes;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+public class QRCodeReader {
+
+	@SuppressWarnings("unused")
+	private static String decodeQRCode(File qrCodeimage) throws IOException {
+		BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+		LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+		try {
+			Result result = new MultiFormatReader().decode(bitmap);
+			return result.getText();
+		} catch (NotFoundException e) {
+			System.out.println("There is no QR code in the image");
+			return null;
+		}
+	}
+
+}
